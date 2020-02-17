424ed5106b970b6b0b2608a3a4d49c9276cdf74e
FabiusE <fabius.engel@msg.group>
2020-02-17 20:26:38 +0100

    Init commit: Codebase for dockerizing a react nativ app

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World from Fabius for his Master Thesis
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
